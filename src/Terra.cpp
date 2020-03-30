6f2bc939330fe9d82f95bc8d8b28f2bbdf52fb6a
Schitrus <schitrus@gmail.com>
2020-03-30 23:42:05 +0200

    Added audio

--- src/Terra.cpp
+++ src/Terra.cpp
@@ -3,6 +3,45 @@
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "portaudio/portaudio.h" 
+
+#define SAMPLE_RATE (44100)
+
+
+typedef struct
+{
+	float left_phase;
+	float right_phase;
+}
+paTestData;
+
+static paTestData data;
+
+static int audio_callback(const void* inputBuffer, void* outputBuffer,
+	unsigned long framesPerBuffer,
+	const PaStreamCallbackTimeInfo* timeInfo,
+	PaStreamCallbackFlags statusFlags,
+	void* userData) {
+	/* Cast data passed through stream to our structure. */
+	paTestData* data = (paTestData*)userData;
+	float* out = (float*)outputBuffer;
+	unsigned int i;
+	(void)inputBuffer; /* Prevent unused variable warning. */
+
+	for (i = 0; i < framesPerBuffer; i++)
+	{
+		*out++ = data->left_phase;  /* left */
+		*out++ = data->right_phase;  /* right */
+		/* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+		data->left_phase += 0.01f;
+		/* When signal reaches top, drop back down. */
+		if (data->left_phase >= 1.0f) data->left_phase -= 2.0f;
+		/* higher pitch so we can distinguish left and right. */
+		data->right_phase += 0.03f;
+		if (data->right_phase >= 1.0f) data->right_phase -= 2.0f;
+	}
+	return 0;
+}
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -53,6 +92,35 @@ int main() {
 		goto exit;
 	}
 
+	if (Pa_Initialize() != paNoError) { 
+		std::cerr << "Failed to initialize PortAudio" << std::endl;
+		goto exit; 
+	}
+
+	PaStream* stream;
+	PaError err;
+
+	/* Open an audio I/O stream. */
+	err = Pa_OpenDefaultStream(&stream,
+		0,          /* no input channels */
+		2,          /* stereo output */
+		paFloat32,  /* 32 bit floating point output */
+		SAMPLE_RATE,
+		256,        /* frames per buffer, i.e. the number
+						   of sample frames that PortAudio will
+						   request from the callback. Many apps
+						   may want to use
+						   paFramesPerBufferUnspecified, which
+						   tells PortAudio to pick the best,
+						   possibly changing, buffer size.*/
+		audio_callback, /* this is your callback function */
+		&data); /*This is a pointer that will be passed to
+						   your callback*/
+	if (err != paNoError) goto exit;
+
+	err = Pa_StartStream(stream);
+	if (err != paNoError) goto exit;
+
 	while (!glfwWindowShouldClose(window)) {
 		double time = glfwGetTime();
 		processInput(window);
@@ -63,6 +131,14 @@ int main() {
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	err = Pa_StopStream(stream);
+	if (err != paNoError) goto exit;
+
+	err = Pa_CloseStream(stream);
+	if (err != paNoError) goto exit;
+
+	Pa_Terminate();
 
 	glfwDestroyWindow(window);
 
